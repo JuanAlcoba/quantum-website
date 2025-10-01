aa9094241b9b2cc54b9edd4ac3ec7fe911d8fd6b
Juan Alcoba <jalcoba@innexus.ar>
2025-09-30 22:57:02 -0300

    Favicons and og img implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,48 @@ export const metadata: Metadata = {
   keywords: ["Quantum Metal", "Metal", "Banda de Metal", "Rio Cuarto", "Cordoba", "Argentina", 
     "Power Metal", "Heavy Metal", "Melodic Metal", "Javier Barrozo", "Rock"
   ],
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    shortcut: ['/favicon.ico'],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  openGraph: {
+    title: "Quantum Metal",
+    description: "Banda de Metal de Cba - Argentina",
+    url: "https://quantumoficial.com.ar",
+    siteName: "Quantum Metal",
+    images: [
+      {
+        url: "https://quantumoficial.com.ar/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Quantum Metal",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://quantumoficial.com.ar",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  other: {
+    "og:image:type": "image/png",
+    "og:image:secure_url": "https://quantumoficial.com.ar/og-image.jpg",
+    "twitter:card": "summary_large_image",
+    "twitter:image": "https://quantumoficial.com.ar/og-image.jpg",
+  },
 }
 
 export default function RootLayout({
